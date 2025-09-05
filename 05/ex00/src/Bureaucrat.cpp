1224d93f22e796d80d69525b43754debf6f2a005
Loic Gerard <lgerard@c1r3p2.42perpignan.fr>
2025-09-05 18:44:16 +0200

    version 42 05/09/2025 finalisation de base pour classe Bureaucrat

--- 05/ex00/src/Bureaucrat.cpp
+++ 05/ex00/src/Bureaucrat.cpp
@@ -6,13 +6,14 @@
 /*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 15:21:18 by lgerard           #+#    #+#             */
-/*   Updated: 2025/09/05 17:40:22 by lgerard          ###   ########.fr       */
+/*   Updated: 2025/09/05 18:18:26 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include <string>
 #include <iostream>
+
 
 // ****************************************************************************
 // Constructors and destructor
@@ -22,28 +23,64 @@ Bureaucrat::Bureaucrat( void )
 : 	name("John DOE"),
 	grade(150)
 {
-	std::cout << "Bureaucrat default contructor executed";
+	std::cout << "Bureaucrat default constructor executed" <<std::endl;
 }
 
 Bureaucrat::Bureaucrat(std::string name, unsigned int grade)
 :	name(name),
 	grade(grade)
 {
-		std::cout << "Bureaucrat default contructor executed";
+	std::cout << "Bureaucrat default constructor executed" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+:	name(other.name),
+	grade(other.grade)
+{
+	std::cout << "Bureaucrat copy constructor executed" << std::endl;
 }
 
 Bureaucrat::~Bureaucrat( void )
 {
-	std::cout << "Bureaucrat destructor executed";	
+	std::cout << "Bureaucrat destructor executed" << std::endl;	
 }
+
 
 // ****************************************************************************
 // overload of assignation operator
 // ****************************************************************************
 
 Bureaucrat&	Bureaucrat::operator=( const Bureaucrat& burct )
+{	
+	std::cout << "Bureaucrat assignation overload executed" << std::endl;	
+	if (this != &burct)
+	{
+		this->grade = burct.grade;
+	}
+}
+
+
+// ****************************************************************************
+// member functions
+// ****************************************************************************
+
+std::string		Bureaucrat::get_name( void ) const
 {
-	this->name = burct.name;
-	this->grade = burct.grade;
-	
+	return (this->name);
+}
+
+unsigned int	Bureaucrat::get_grade( void ) const
+{
+	return	(this->grade);
+}
+
+
+// ****************************************************************************
+// overload of ostream operator
+// ****************************************************************************
+
+std::ostream & operator<<(std::ostream & o, Bureaucrat const & burct)
+{
+	o << burct.get_name() << ", bureaucrat grade " << burct.get_grade();
+	return (o)
 }
--- 05/ex00/src/Bureaucrat.hpp
+++ 05/ex00/src/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 15:16:57 by lgerard           #+#    #+#             */
-/*   Updated: 2025/09/05 17:34:08 by lgerard          ###   ########.fr       */
+/*   Updated: 2025/09/05 18:14:48 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,19 @@ class Bureaucrat
 {
 	public:
 		Bureaucrat( void );
-		Bureaucrat (std::string name, unsigned int grad);
+		Bureaucrat (std::string name, unsigned int grade);
+		Bureaucrat ( const Bureaucrat & other );
 		~Bureaucrat ( void );
-		Bureaucrat& operator=(const Bureaucrat& burct);
+		Bureaucrat& operator=(const Bureaucrat & burct);
+
+		std::string		get_name( void ) const;
+		unsigned int	get_grade( void ) const;
 		
 	private:
 		const std::string	name;
 		unsigned int		grade;
 		
 };
+
+std::ostream & operator<<(std::ostream & o, Bureaucrat const & burct);
 #endif;
