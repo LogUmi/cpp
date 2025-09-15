b6752f8a81d38700485445493043b8630db20795
LogUmi <polarismoins@gmail.com>
2025-09-15 14:00:00 +0200

    version home 15/09 ex03 cpp05 a tester

--- 05/ex03/src/Intern.cpp
+++ 05/ex03/src/Intern.cpp
@@ -6,7 +6,99 @@
 /*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 14:21:56 by lgerard           #+#    #+#             */
-/*   Updated: 2025/09/14 14:22:01 by lgerard          ###   ########.fr       */
+/*   Updated: 2025/09/15 13:57:12 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Intern.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "AForm.hpp"
+
+// ****************************************************************************
+// Constructors and destructor
+// ****************************************************************************
+
+Intern::Intern ( void )
+{
+	//std::cout << "Intern default constructor executed" <<std::endl;
+	return ;
+}
+
+Intern::Intern ( Intern const & other )
+{
+	(void) other;
+	//std::cout << "Intern copy constructor executed" <<std::endl;
+	return ;
+}
+
+Intern::~Intern ( void )
+{
+	//std::cout << "Intern destructor executed" <<std::endl;
+	return ;
+}
+
+// ****************************************************************************
+// overload of assignation operator
+// ****************************************************************************
+
+Intern & Intern::operator=( const Intern & other )
+{
+	(void)other;
+	return (*this);
+}
+
+// ****************************************************************************
+// member functions
+// ****************************************************************************
+
+AForm* 	Intern::createShrubbery( std::string const & target )
+{
+	return ( new ShrubberyCreationForm( target ) );
+}
+
+AForm*		Intern::createRobotomy( std::string const & target )
+{
+	return ( new RobotomyRequestForm( target ) );
+}
+
+AForm*		Intern::createPresidential( std::string const & target )
+{
+	return (new PresidentialPardonForm( target ) );
+}
+
+AForm* Intern::makeForm(std::string name, std::string target)
+{
+	std::string requests[4] =  
+	{"shrubbery creation", "robotomy request", "presidential pardon", ""};
+	AForm* (Intern::*creators[3])(std::string const&) = 
+	{&Intern::createShrubbery, &Intern::createRobotomy, &Intern::createPresidential};
+	int i = 0;
+    AForm* form = NULL;
+
+	while (!requests[i].empty())
+	{
+		if (name == requests[i])
+		{
+			form = (this->*creators[i])( target );
+			break ;
+		}
+		i++;
+	}
+	if (requests[i].empty())
+		throw WrongFormException();
+	std::cout << "Intern creates " << form->get_name() << std::endl;
+	return (form);
+}
+
+// ****************************************************************************
+// ****************************************************************************
+// Exceptions
+// ****************************************************************************
+// ****************************************************************************
+
+const char * Intern::WrongFormException::what( void ) const throw()
+{
+	return ("Intern can't create an unknown form name");
+}
--- 05/ex03/src/Intern.hpp
+++ 05/ex03/src/Intern.hpp
@@ -6,7 +6,34 @@
 /*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 14:22:09 by lgerard           #+#    #+#             */
-/*   Updated: 2025/09/14 14:22:23 by lgerard          ###   ########.fr       */
+/*   Updated: 2025/09/15 13:58:34 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+# include "AForm.hpp"
+
+class Intern
+{
+	private:
+		AForm* 		createShrubbery( std::string const & target );
+       	AForm*		createRobotomy( std::string const & target );
+       	AForm*		createPresidential( std::string const & target );
+
+	public:
+					Intern ( void );
+					Intern ( Intern const & other );
+					~Intern ( void );
+		Intern & 	operator=( Intern const & other );
+
+		AForm*		makeForm(std::string name, std::string target);
+
+		class WrongFormException: std::exception
+		{
+			public:
+				virtual const char * what( void ) const throw();
+		};
+};
+#endif
