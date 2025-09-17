38bf845711d1eddb88eb3faa2bd78940a43720b7
Loic Gerard <lgerard@c1r1p7.42perpignan.fr>
2025-09-17 19:00:22 +0200

    version 42 du 17/09 avancee ex00 cpp06

--- 06/ex00/src/ScalarConverter.cpp
+++ 06/ex00/src/ScalarConverter.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 20:27:59 by lgerard           #+#    #+#             */
-/*   Updated: 2025/09/16 21:15:16 by lgerard          ###   ########.fr       */
+/*   Updated: 2025/09/17 17:56:55 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,73 @@ ScalarConverter & ScalarConverter::operator=( const ScalarConverter & other )
 // member functions
 // ****************************************************************************
 
-void	ScalarConverter::convert( const std::string & literal )
+void	ScalarConverter::convert( const std::string & arg )
 {
+	char	c = 0;
+	int		i = 0;
+	float	f = 0.0;
+	double	d = 0.0;
+	int 	status[4] = {0, 0, 0, 0};
+	
+	// ********** char section **********
+	if( arg.size() > 1 )
+		status[0] = 2;
+	else
+		c = arg[0];
+	// ********** int section **********
+	try
+	{
+		i = std::stoi( arg, NULL, 10);
+	}
+	catch(const std::invalid_argument& e)
+	{
+		status[1] = 2;
+	}
+	catch(const std::out_of_range& e)
+	{
+		status[1] = 2;
+	}
+	if (status[0] != 0 && status[1] == 0)
+	{
+		if (i < 32 || i == 127)
+			status[0] = 1;
+		else if ( i < 127)
+		{
+			c = static_cast<char>(i);
+			status[0] = 0;
+		}
+		else
+			status[0] = 2;	
+	}
+	// ********** float section **********
+	try
+	{
+		f = std::stof( arg, NULL );
+	}
+	catch(const std::invalid_argument& e)
+	{
+		status[1] = 2;
+	}
+	catch(const std::out_of_range& e)
+	{
+		status[1] = 1;
+	}
+	
+	// ********** double section **********
+	try
+	{
+		d = std::stod( arg, NULL );
+	}
+	catch(const std::invalid_argument& e)
+	{
+		status[1] = 2;
+	}
+	catch(const std::out_of_range& e)
+	{
+		status[1] = 1;
+	}
+
+
 	
 	return ;
 }
