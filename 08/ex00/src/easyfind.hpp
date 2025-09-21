f1406df5605060f549e03d89130c1872814a4105
LogUmi <polarismoins@gmail.com>
2025-09-21 21:22:44 +0200

    version home 21/09 cpp08 ex00 a verifier linux

---
+++ 08/ex00/src/easyfind.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 16:27:41 by lgerard           #+#    #+#             */
+/*   Updated: 2025/09/21 20:48:30 by lgerard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <stdexcept>
+
+template < typename T >
+typename T::const_iterator	easyfind(const T & data, const int n);
+
+# include "easyfind.tpp"
+
+#endif
